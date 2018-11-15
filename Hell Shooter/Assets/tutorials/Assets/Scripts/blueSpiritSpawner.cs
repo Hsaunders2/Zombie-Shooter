bd57cd7b5862b945126888888d500c94ca0f0ca7
Hsaunders2 <Hsaunders2@users.noreply.github.com>
2018-11-15 19:51:15 +0000

    Blue Spirit Enemy
    Made Blue Spirit Sprites.
    Made animation.
    added health and hit boxes.
    created spawner for blue spirit.

---
+++ Hell Shooter/Assets/tutorials/Assets/Scripts/blueSpiritSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blueSpiritSpawner : MonoBehaviour
+{
+
+    public GameObject blueSpiritPrefab;
+    public Transform spawn;
+    public Transform player;
+    public float spawnTime = 0.5f;
+
+    private bool isSpawning = false;
+
+    private void SetSpawning()
+    {
+        isSpawning = false;
+    }
+
+    private void Spawn()
+    {
+        isSpawning = true;
+        GameObject blueSpirit = Instantiate(blueSpiritPrefab, spawn.position, spawn.rotation);
+
+        blueSpirit.GetComponent<MoveTowardsTarget>().target = player;
+
+        Invoke("SetSpawning", spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isSpawning)
+        {
+            Spawn();
+        }
+    }
+}
